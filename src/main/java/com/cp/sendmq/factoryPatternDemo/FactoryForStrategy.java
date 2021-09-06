fadb27ae5fa289a79c13db292fcf7885b17bf7e8
yang <123>
2021-09-06 18:02:42 +0800

    策略工程模式

--- src/main/java/com/cp/sendmq/factoryPatternDemo/FactoryForStrategy.java
+++ src/main/java/com/cp/sendmq/factoryPatternDemo/FactoryForStrategy.java
@@ -3,6 +3,7 @@ package com.cp.sendmq.factoryPatternDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,8 +20,10 @@ public class FactoryForStrategy {
     //接口和抽象都可用
     @Autowired
     Map<String, ReceiveHandle2> strategys = new ConcurrentHashMap<>(3);
-
+    @Autowired
+    Map<String, ReceiveHandle2> strategys2 = new HashMap<>(3);
     public ReceiveHandle2 getStrategy(String component) throws Exception {
+        ReceiveHandle2 receiveHandle2 = strategys2.get(component);
         ReceiveHandle2 strategy = strategys.get(component);
         if (strategy == null) {
             throw new RuntimeException("no strategy defined");
